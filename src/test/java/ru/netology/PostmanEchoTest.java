e3f3903a91a5342a34c2ef826b4a35a716d2dba0
ksenny <cnhfecbrf@mail.ru>
2026-05-25 22:37:28 +0300

    true tests

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -17,6 +17,6 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("wrong data"));
+                .body("data", equalTo("some data"));
     }
 }
